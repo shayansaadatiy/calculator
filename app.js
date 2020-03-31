9d9c5ad9659653af3d0d5bbc95f75f7f8ceff9d5
shayan saadati <shayansaadatiy@gmail.com>
2020-03-31 21:04:00 +0430

    add 4 function

---
+++ app.js
@@ -0,0 +1,21 @@
+const defaultResult = 0;
+let currentResult = 0;
+
+function add (number1,numbr2){
+    const result = number1 + number2;
+    return result;
+}
+function subtract (number1,number2){
+    const result = number1 - number2;
+    return result;
+}
+function multyply (number1,number2){
+    const result = number1 * number2;
+    return result;
+}
+function divide (number1 , number2){
+    const result = number1 / number2;
+    return result;
+}
+
+outPutResult (currentResult , calculationMassage)
